ae61bc3d370a3d3bda10e5ebed172aca2e4071be
Muhamad Habib <muhamadhabib1000@gmail.com>
2018-11-28 21:37:41 +0700

    Add class TaskDetailActivity
    Menambah class TaskDetailActivity pada project note it

---
+++ app/src/main/java/com/noteit/mkpl_android_note_it/TaskDetailActivity.java
@@ -0,0 +1,13 @@
+package com.noteit.mkpl_android_note_it;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.DatePicker;
+
+public class TaskDetailActivity extends AppCompatActivity implements
+        DatePickerDialog.OnDateSetListener  {
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+}
