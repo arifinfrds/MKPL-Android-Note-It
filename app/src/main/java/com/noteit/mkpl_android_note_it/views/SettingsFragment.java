36935652c9e289642b6be56817b4af4a2fd297ae
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2018-11-28 17:43:56 +0700

    added base ui project

---
+++ app/src/main/java/com/noteit/mkpl_android_note_it/views/SettingsFragment.java
@@ -0,0 +1,75 @@
+package com.noteit.mkpl_android_note_it.views;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceManager;
+
+import com.noteit.mkpl_android_note_it.R;
+
+public class SettingsFragment extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    SharedPreferences mSharedPreferences;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mSharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updatePreference(findPreference(key), key);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); ++i) {
+            Preference preference = getPreferenceScreen().getPreference(i);
+            if (preference instanceof PreferenceGroup) {
+                PreferenceGroup preferenceGroup = (PreferenceGroup) preference;
+                for (int j = 0; j < preferenceGroup.getPreferenceCount(); ++j) {
+                    Preference singlePref = preferenceGroup.getPreference(j);
+                    updatePreference(singlePref, singlePref.getKey());
+                }
+            } else {
+                updatePreference(preference, preference.getKey());
+            }
+        }
+    }
+
+    private void updatePreference(Preference preference, String key) {
+        if (preference == null) return;
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            String value = mSharedPreferences.getString(preference.getKey(), "");
+            setPreferenceSummary(preference, value);
+        }
+    }
+
+    private void setPreferenceSummary(Preference preference, String value) {
+        if (preference instanceof ListPreference) {
+            // For list preferences, figure out the label of the selected value
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(value);
+            if (prefIndex >= 0) {
+                // Set the summary to that label
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        mSharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        super.onDestroyView();
+    }
+
+}
