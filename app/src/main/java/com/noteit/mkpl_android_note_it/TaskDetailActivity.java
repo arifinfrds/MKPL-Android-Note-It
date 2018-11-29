5987a6f0640b43326e821bda4c4ad2aec1e47357
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2018-11-29 15:49:44 +0700

    added feature to show notes

--- app/src/main/java/com/noteit/mkpl_android_note_it/TaskDetailActivity.java
+++ app/src/main/java/com/noteit/mkpl_android_note_it/TaskDetailActivity.java
@@ -4,9 +4,29 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.noteit.mkpl_android_note_it.views.TaskTitleView;
 
 public class TaskDetailActivity extends AppCompatActivity implements
         DatePickerDialog.OnDateSetListener {
+
+    public static final String EXTRA_PARCEL = "EXTRA_PARCEL";
+    public static final String EXTRA_SAVED = "EXTRA_PARCEL";
+    public static String EXTRA_ID = "EXTRA_ID";
+    public static String EXTRA_DESCRIPTION = "EXTRA_DESCRIPTION";
+    public static String EXTRA_IS_PRIORITY = "EXTRA_IS_PRIORITY";
+    public static String EXTRA_IS_COMPLETE = "EXTRA_IS_COMPLETE";
+    public static String EXTRA_DUE_DATE = "EXTRA_DUE_DATE";
+
+    private TaskTitleView taskTitleView;
+    private TextView dateView;
+    private ImageView priorityView;
+
+    private String description;
+    private int priority, iscompleted;
+    private long id, datetime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
